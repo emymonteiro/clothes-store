e1c7579ac8f11ad86a5911c481caeb6002bbaa03
Emilly <emymonteiro.sweetie@gmail.com>
2023-06-01 18:18:22 -0400

    create routes and add product page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Product from '../components/Product.svelte';
+
+	export let data;
+</script>
+
+<section class="py-16">
+	<div class="container mx-auto">
+		<div
+			class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]
+		max-w-sm mx-auto md:max-w-none md:mx-0"
+		>
+			{#each data.filteredProducts as item}
+				<Product product={item} />
+			{/each}
+		</div>
+	</div>
+</section>
