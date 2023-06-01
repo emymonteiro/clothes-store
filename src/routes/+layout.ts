4a09c4e5c3420fa8b649cfb0f91eb833b3ec4d52
Emilly <emymonteiro.sweetie@gmail.com>
2023-06-01 18:28:49 -0400

    Updates

--- src/routes/+layout.ts
+++
@@ -1,3 +0,0 @@
-import "../app.postcss"
-import '@skeletonlabs/skeleton/themes/theme-vintage.css';
-export const prerender = true;
--- src/routes/product/[id]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-</script>
-
-<h1>Hello world</h1>
-
-<pre>
-    {JSON.stringify($page, null, 2)}
-</pre>
